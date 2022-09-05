9371ff9e11f9b0aebbcb00504bac9676ef3ae743
no dox ples <nice@osint.com>
2022-09-05 11:48:19 +0200

    replaced tabs with spaces

--- poc.c
+++ poc.c
@@ -4,22 +4,22 @@
 #include <stdlib.h>
 
 int main() {
-	char buf[256];
+    char buf[256];
 
-	printf("now you see me\n");
+    printf("now you see me\n");
 
-	char secret[14] = "now you";
-	char final[] = "\xa3\xe7\xec\xed\xa4\xf7";
-	for (int i=0; i<6; i++) {
-		final[i] ^= 0x83;
-	}
+    char secret[14] = "now you";
+    char final[] = "\xa3\xe7\xec\xed\xa4\xf7";
+    for (int i=0; i<6; i++) {
+        final[i] ^= 0x83;
+    }
 
-	strcat(secret, final);
+    strcat(secret, final);
 
     read(1, buf, 128);
     buf[strlen(buf)-1] = '\x00';
 
-	if (!strcmp(buf, secret)) {
-		system("exit");
-	}
+    if (!strcmp(buf, secret)) {
+        system("exit");
+    }
 }
